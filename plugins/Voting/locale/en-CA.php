dd13626ae245cf210288483a4c1af3fd17225d24
Todd Burry <todd@vanillaforums.com>
2010-11-25 14:14:55 -0500

    Added the voting plugin.

---
+++ plugins/Voting/locale/en-CA.php
@@ -0,0 +1,27 @@
+<?php if (!defined('APPLICATION')) exit();
+/* Change "Bookmarks" to be "Follows", and other fun translation changes. */
+  
+// Discussion / Question
+$Definition['Start a New Discussion'] = 'Ask a Question';
+$Definition['Discussions'] = 'Questions';
+$Definition['All Discussions'] = 'All Questions';
+$Definition['My Discussions'] = 'Mine';
+$Definition['Delete Discussion'] = 'Delete Question';
+$Definition['No discussions were found.'] = 'No questions were found.';
+$Definition['Discussion Title'] = 'Question Title';
+$Definition['Post Discussion'] = 'Post Question';
+$Definition['%1$s started a %8$s.'] = '%1$s asked a %8$s.';
+$Definition['%1$s commented on %4$s %8$s.'] = '%1$s answered %4$s %8$s.';
+$Definition['discussion'] = 'question';
+
+// Bookmark / Follow
+$Definition['Bookmarked Discussions'] = 'Following';
+$Definition['My Bookmarks'] = 'Following';
+
+// Comment / Answer
+$Definition['Write Comment'] = 'Write Answer';
+$Definition['Post Comment'] = 'Post Answer';
+$Definition['%1$s more comments'] = '%s more';
+$Definition['%1$s older comments'] = '%1$s older';
+$Definition['Comment'] = 'Answer';
+$Definition['Comments'] = 'Answers';
