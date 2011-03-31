66651b71ae6226011a47185640137c9514640845
Todd Burry <todd@vanillaforums.com>
2011-03-31 14:03:40 -0400

    Q&A plugin development.

--- plugins/QnA/class.qna.plugin.php
+++ plugins/QnA/class.qna.plugin.php
@@ -6,7 +6,7 @@
 
 // Define the plugin:
 $PluginInfo['QnA'] = array(
-   'Name' => 'Q&A (Question & Answer)',
+   'Name' => 'Q&A',
    'Description' => "Allows users to designate a discussion as a question and then accept one or more of the comments as an answer.",
    'Version' => '1.0b',
    'RequiredApplications' => array('Vanilla' => '2.0.18a1'),
@@ -18,8 +18,11 @@ $PluginInfo['QnA'] = array(
 class QnAPlugin extends Gdn_Plugin {
    /// PROPERTIES ///
 
-
    /// METHODS ///
+
+   public function Setup() {
+      $this->Structure();
+   }
 
    public function Structure() {
       Gdn::Structure()
@@ -31,20 +34,37 @@ class QnAPlugin extends Gdn_Plugin {
          ->Table('Comment')
          ->Column('QnA', array('Accepted', 'Rejected'), NULL)
          ->Set();
+
+      Gdn::SQL()->Replace(
+         'ActivityType',
+         array('AllowComments' => '0', 'RouteCode' => 'question', 'Notify' => '1', 'Public' => '0', 'ProfileHeadline' => '', 'FullHeadline' => ''),
+         array('Name' => 'QuestionAnswer'));
+      Gdn::SQL()->Replace(
+         'ActivityType',
+         array('AllowComments' => '0', 'RouteCode' => 'answer', 'Notify' => '1', 'Public' => '0', 'ProfileHeadline' => '', 'FullHeadline' => ''),
+         array('Name' => 'AnswerAccepted'));
    }
 
 
    /// EVENTS ///
 
-   public function DiscussionController_AfterCommentBody_Handler($Sender, $Args) {
+   /**
+    *
+    * @param Gdn_Controller $Sender
+    * @param array $Args
+    */
+   public function Base_CommentOptions_Handler($Sender, $Args) {
       $Discussion = GetValue('Discussion', $Args);
       $Comment = GetValue('Comment', $Args);
 
-      if (!$Discussion || !$Comment)
+      if (!$Discussion || !$Comment || !GetValue('Type', $Discussion) == 'question')
          return;
 
       // Check permissions.
-      if (!(Gdn::Session()->UserID == GetValue('InsertUserID', $Discussion) || Gdn::Session()->CheckPermission('Garden.Moderation.Manage')))
+      $CanAccept = Gdn::Session()->CheckPermission('Garden.Moderation.Manage');
+      $CanAccept |= Gdn::Session()->UserID == GetValue('InsertUserID', $Discussion) && Gdn::Session()->UserID != GetValue('InsertUserID', $Comment);
+      
+      if (!$CanAccept)
          return;
 
       $QnA = GetValue('QnA', $Comment);
@@ -54,11 +74,15 @@ class QnAPlugin extends Gdn_Plugin {
       // Write the links.
       $Query = http_build_query(array('commentid' => GetValue('CommentID', $Comment), 'tkey' => Gdn::Session()->TransientKey()));
 
-      echo '<div class="Inset QnA-Form">'.
-            T('Did this answer the question?').
-            ' '.Anchor(T('Yes'), '/discussion/qna/accept?'.$Query, array('class' => 'Button QnA-Yes')).
-            ' '.Anchor(T('No'), '/discussion/qna/reject?'.$Query, array('class' => 'Button QnA-No')).
-         '</div>';
+      echo '<span>'.Anchor(T('Accept', 'Accept'), '/discussion/qna/accept?'.$Query, array('class' => 'QnA-Yes', 'title' => T('Accept this answer.'))).'</span>'.
+         '<span>'.Anchor(T('Reject', 'Reject'), '/discussion/qna/reject?'.$Query, array('class' => 'QnA-No', 'title' => T('Reject this answer.'))).'</span>';
+
+      static $InformMessage = TRUE;
+
+      if ($InformMessage && Gdn::Session()->UserID == GetValue('InsertUserID', $Discussion) && in_array(GetValue('QnA', $Discussion), array('', 'Answered'))) {
+         $Sender->InformMessage(T('Click accept or reject beside an answer.'), 'Dismissable');
+         $InformMessage = FALSE;
+      }
    }
 
    public function Base_CommentInfo_Handler($Sender, $Args) {
@@ -68,9 +92,38 @@ class QnAPlugin extends Gdn_Plugin {
 
       $QnA = GetValueR('Comment.QnA', $Args);
 
-      if ($QnA) {
+      if ($QnA && ($QnA == 'Accepted' || Gdn::Session()->CheckPermission('Garden.Moderation.Manage'))) {
          $Title = sprintf(T('This answer was %s.'), T('Q&A '.$QnA, $QnA));
          echo '<div class="QnA-Box QnA-'.$QnA.'" title="'.htmlspecialchars($Title).'"><div>'.$Title.'</div></div>';
+      }
+   }
+
+   public function CommentModel_BeforeNotification_Handler($Sender, $Args) {
+      $ActivityModel = $Args['ActivityModel'];
+      $Comment = (array)$Args['Comment'];
+      $CommentID = $Comment['CommentID'];
+      $Discussion = (array)$Args['Discussion'];
+
+      $ActivityID = $ActivityModel->Add(
+         $Comment['InsertUserID'],
+         'QuestionAnswer',
+         Anchor(Gdn_Format::Text($Discussion['Name']), "discussion/comment/$CommentID/#Comment_$CommentID"),
+         $Discussion['InsertUserID'], 
+         '',
+         "/discussion/comment/$CommentID/#Comment_$CommentID");
+      $ActivityModel->QueueNotification($ActivityID, '', 'first');
+   }
+
+   /**
+    * @param CommentModel $Sender
+    * @param array $Args
+    */
+   public function CommentModel_BeforeUpdateCommentCount_Handler($Sender, $Args) {
+      $Discussion =& $Args['Discussion'];
+
+      // Mark the question as answered.
+      if (strtolower($Discussion['Type']) == 'question' && !$Discussion['Sink'] && !in_array($Discussion['QnA'], array('Answered', 'Accepted')) && $Discussion['InsertUserID'] != Gdn::Session()->UserID) {
+         $Sender->SQL->Set('QnA', 'Answered');
       }
    }
 
@@ -109,9 +162,25 @@ class QnAPlugin extends Gdn_Plugin {
          // Update the discussion.
          if (!$Discussion['QnA'] || $Discussion['QnA'] = 'Answered')
             Gdn::SQL()->Put('Discussion', array('QnA' => $QnA), array('DiscussionID' => $Comment['DiscussionID']));
+
+         // Record the activity.
+         AddActivity(
+            Gdn::Session()->UserID,
+            'AnswerAccepted',
+            Anchor(Gdn_Format::Text($Discussion['Name']), "/discussion/{$Discussion['DiscussionID']}/".Gdn_Format::Url($Discussion['Name'])),
+            $Comment['InsertUserID'],
+            "/discussion/comment/{$Comment['CommentID']}/#Comment_{$Comment['CommentID']}",
+            TRUE
+         );
       }
 
       Redirect("/discussion/comment/{$Comment['CommentID']}#Comment_{$Comment['CommentID']}");
+   }
+
+   public function DiscussionModel_BeforeGet_Handler($Sender, $Args) {
+      if ($QnA = Gdn::Request()->Get('qna')) {
+         $Args['Wheres']['QnA'] = $QnA;
+      }
    }
 
    /**
@@ -130,29 +199,54 @@ class QnAPlugin extends Gdn_Plugin {
          return;
 
       $QnA = GetValue('QnA', $Discussion);
+      $Title = '';
       switch ($QnA) {
          case '':
          case 'Rejected':
+            $Text = 'Question';
             $QnA = 'Question';
             break;
          case 'Answered':
+            $Text = 'Answered';
+            if (GetValue('InsertUserID', $Discussion) == Gdn::Session()->UserID) {
+               $QnA = 'Answered Alert';
+               $Title = ' title="'.T("Someone's answered your question. You need to accept/reject the answer.").'"';
+            }
+            break;
          case 'Accepted':
-            $QnA = 'Answered';
+            $Text = 'Answered';
+            $Title = ' title="'.T("This question's answer has been accepted.").'"';
             break;
          default:
             $QnA = FALSE;
       }
       if ($QnA) {
-         echo '<span class="Tag Tag-'.$QnA.'">'.T("Q&A $QnA", $QnA).'</span> ';
+         echo '<span class="Tag QnA-Tag-'.$QnA.'"'.$Title.'>'.T("Q&A $Text", $Text).'</span> ';
+      }
+   }
+
+   /**
+    * @param Gdn_Controller $Sender
+    * @param array $Args
+    */
+   public function NotificationsController_BeforeInformNotifications_Handler($Sender, $Args) {
+      $Path = trim($Sender->Request->GetValue('Path'), '/');
+      if (preg_match('`^(vanilla/)?discussion[^s]`i', $Path))
+         return;
+
+      // Check to see if the user has answered questions.
+      $Count = Gdn::SQL()->GetCount('Discussion', array('Type' => 'Question', 'InsertUserID' => Gdn::Session()->UserID, 'QnA' => 'Answered'));
+      if ($Count > 0) {
+         $Sender->InformMessage(FormatString(T('You have answered questions', 'You have answered <a href="{/discussions/mine?qna=Answered,url}">questions</a>. Make sure you accept/reject the answers.')), '');
       }
    }
 
    public function PostController_BeforeBodyInput_Handler($Sender, $Args) {
       $Form = $Sender->Form;
 
-      echo '<p>',
-         T('You can either as a question or start a regular discussion.', 'You can either as a question or start a regular discussion. Choose what you want to do below.'),
-         '</p>';
+      echo '<div class="P">',
+         T('You can either ask a question or start a discussion.', 'You can either ask a question or start a discussion. Choose what you want to do below.'),
+         '</div>';
 
       echo '<div class="P">',
          $Form->RadioList('Type', array('Question' => 'Question', 'Discussion' => 'Discussion')),
