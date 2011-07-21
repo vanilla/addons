c693baff09145681404f7416fa55be62bd4c857b
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-07-21 12:08:56 -0400

    fix requirements

--- plugins/Quotes/class.quotes.plugin.php
+++ plugins/Quotes/class.quotes.plugin.php
@@ -14,7 +14,7 @@ $PluginInfo['Quotes'] = array(
    'Description' => "This plugin allows users to quote each other easily.",
    'Version' => '1.2.2',
    'MobileFriendly' => TRUE,
-   'RequiredApplications' => array('Vanilla'),
+   'RequiredApplications' => array('vanilla' => "2.0"),
    'RequiredTheme' => FALSE, 
    'RequiredPlugins' => FALSE,
    'HasLocale' => TRUE,
