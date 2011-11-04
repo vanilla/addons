a0ff036a961378f53e9e5f3b7ed2ad12a9d75032
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-11-04 15:03:47 -0400

    Fix class naming to prevent clashes.

--- plugins/ButtonBar/views/buttonbar.php
+++ plugins/ButtonBar/views/buttonbar.php
@@ -1,11 +1,11 @@
 <div class="ButtonBar">
-   <div class="Bold"><span>bold</span></div>
-   <div class="Italic"><span>italic</span></div>
-   <div class="Underline"><span>underline</span></div>
-   <div class="Strike"><span>strike</span></div>
-   <div class="Code"><span>code</span></div>
-   <div class="Image"><span>image</span></div>
-   <div class="Url"><span>url</span></div>
-   <div class="Quote"><span>quote</span></div>
-   <div class="Spoiler"><span>spoiler</span></div>
+   <div><span>bold</span></div>
+   <div><span>italic</span></div>
+   <div><span>underline</span></div>
+   <div><span>strike</span></div>
+   <div><span>code</span></div>
+   <div><span>image</span></div>
+   <div><span>url</span></div>
+   <div><span>quote</span></div>
+   <div><span>spoiler</span></div>
 </div>
