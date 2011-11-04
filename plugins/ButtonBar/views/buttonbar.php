2929bd3919e355a95460ac378d8fb4197d625802
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-11-04 14:12:21 -0400

    ButtonBar plugin for simple markup.

---
+++ plugins/ButtonBar/views/buttonbar.php
@@ -0,0 +1,11 @@
+<div class="ButtonBar">
+   <div class="Bold"><span>bold</span></div>
+   <div class="Italic"><span>italic</span></div>
+   <div class="Underline"><span>underline</span></div>
+   <div class="Strike"><span>strike</span></div>
+   <div class="Code"><span>code</span></div>
+   <div class="Image"><span>image</span></div>
+   <div class="Url"><span>url</span></div>
+   <div class="Quote"><span>quote</span></div>
+   <div class="Spoiler"><span>spoiler</span></div>
+</div>
