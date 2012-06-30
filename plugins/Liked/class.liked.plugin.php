a7d4b598cd855e1fece02b49ba12324edc9b2601
Mark O'Sullivan <mark@vanillaforums.com>
2012-06-30 18:50:26 -0400

    Fixing Liked plugin to function the new FB way.

--- plugins/Liked/class.liked.plugin.php
+++ plugins/Liked/class.liked.plugin.php
@@ -4,7 +4,7 @@
 $PluginInfo['Liked'] = array(
    'Name' => 'Liked',
    'Description' => 'Adds the facebook like feature to your discussions.',
-   'Version' => '1.5',
+   'Version' => '1.6',
    'Author' => "Gary Mardell",
    'AuthorEmail' => 'gary@vanillaplugins.com',
    'AuthorUrl' => 'http://garymardell.co.uk'
@@ -13,13 +13,23 @@ $PluginInfo['Liked'] = array(
 class LikedPlugin extends Gdn_Plugin {
 	
 	public function DiscussionController_Render_Before(&$Sender) {
-   	$Sender->AddJsFile('http://connect.facebook.net/en_US/all.js#xfbml=1');
+      $FB_SDK = <<<EOD
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+EOD;
+      $Sender->AddAsset('Panel', $FB_SDK);
 	}
 	
-	public function DiscussionController_BeforeDiscussionOptions_Handler(&$Sender) {
-      echo '<fb:like href="';
+	public function DiscussionController_AfterDiscussionBody_Handler(&$Sender) {
+      echo '<div class="fb-like" data-href="';
       echo Gdn_Url::Request(true, true, true);
-      echo '" layout="button_count" width="60" show_faces="false" font="lucida grande"></fb:like>';
+      echo '" data-send="false" data-width="450" data-show-faces="false" data-font="lucida grande"></div>';
 	}
 
    public function Setup() {
