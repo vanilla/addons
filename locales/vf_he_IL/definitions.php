d3b8234f3261ee0f86383573576fa89c401ecb5c
Todd Burry <todd@vanillaforums.com>
2012-07-06 13:35:52 -0400

    Hebrew locale tweaks.

--- locales/vf_he_IL/definitions.php
+++ locales/vf_he_IL/definitions.php
@@ -2,7 +2,7 @@
 
 $LocaleInfo['vf_he_IL'] = array(
    'Locale'       => 'he-IL',
-   'Name'         => 'Hebrew',
-   'Description'  => '',
+   'Name'         => 'עברית (Hebrew, he-IL)',
+   'Description'  => 'Hebrew language translations for Vanilla. Help contribute to this translation be going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/he_IL/">here</a>.',
    'Version'      => '1.0', 
 );
