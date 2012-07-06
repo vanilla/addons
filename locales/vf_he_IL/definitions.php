30d50ad942abd0bcd532a9457b66eacd724861e5
Todd Burry <todd@vanillaforums.com>
2012-07-06 13:19:29 -0400

    vf Hebrew (he_IL) locale from transfix.

---
+++ locales/vf_he_IL/definitions.php
@@ -0,0 +1,8 @@
+<?php
+
+$LocaleInfo['vf_he_IL'] = array(
+   'Locale'       => 'he-IL',
+   'Name'         => 'Hebrew',
+   'Description'  => '',
+   'Version'      => '1.0', 
+);
---
+++ locales/vf_he_IL/foo.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$Definition['Categories'] = 'הקטגוריות';
+$Definition['Discussions'] = 'אחרונים';
+$Definition['Category'] = 'קטגוריה';
+$Definition['Comments'] = 'תגובות';
+$Definition['Cancel'] = 'לבטל';
