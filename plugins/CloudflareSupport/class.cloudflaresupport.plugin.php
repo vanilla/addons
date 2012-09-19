81922c7d8f36934701a293a4a96c264f0dcc3063
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2012-09-19 16:16:51 -0400

    Fix spacing.

--- plugins/CloudflareSupport/class.cloudflaresupport.plugin.php
+++ plugins/CloudflareSupport/class.cloudflaresupport.plugin.php
@@ -2,13 +2,13 @@
 
 /**
  * Cloudflare Support Plugin
- * 
+ *
  * This plugin inspect the incoming request headers and applies CF-Connecting-IP
  * to the request object.
- * 
- * Changes: 
+ *
+ * Changes:
  *  1.0     Initial release
- * 
+ *
  * @author Tim Gunter <tim@vanillaforums.com>
  * @copyright 2003 Vanilla Forums, Inc
  * @license http://www.opensource.org/licenses/gpl-2.0.php GPL
@@ -20,7 +20,7 @@ $PluginInfo['CloudflareSupport'] = array(
    'Description' => 'This plugin modifies the Request object to work with Cloudflare.',
    'Version' => '1.1',
    'RequiredApplications' => array('Vanilla' => '2.1a'),
-   'RequiredTheme' => FALSE, 
+   'RequiredTheme' => FALSE,
    'RequiredPlugins' => FALSE,
    'HasLocale' => FALSE,
    'SettingsUrl' => FALSE,
@@ -31,40 +31,41 @@ $PluginInfo['CloudflareSupport'] = array(
 );
 
 class CloudflareSupportPlugin extends Gdn_Plugin {
-   
+
    const CLOUDFLARE_SOURCE_IPS = array(
-       "204.93.240.0/24", 
-       "204.93.177.0/24", 
-       "199.27.128.0/21", 
-       "173.245.48.0/20", 
-       "103.22.200.0/22", 
-       "141.101.64.0/18", 
-       "108.162.192.0/18",
-       "190.93.240.0/20");
-   
+      "204.93.240.0/24",
+      "204.93.177.0/24",
+      "199.27.128.0/21",
+      "173.245.48.0/20",
+      "103.22.200.0/22",
+      "141.101.64.0/18",
+      "108.162.192.0/18",
+      "190.93.240.0/20");
+
    public function __construct() {
       parent::__construct();
-      
+
       // If cloudflare isn't telling us a client IP, bust outta here!
       $CloudflareClientIP = GetValue('HTTP_CF_CONNECTING_IP', $_SERVER, NULL);
-      if (is_null($CloudflareClientIP)) return;
-      
+      if (is_null($CloudflareClientIP))
+         return;
+
       $RemoteAddress = Gdn::Request()->RemoteAddress();
       $CloudflareRequest = FALSE;
-		foreach (self::CLOUDFLARE_SOURCE_IPS as $CloudflareIPRange) {
-         
+      foreach (self::CLOUDFLARE_SOURCE_IPS as $CloudflareIPRange) {
+
          // Not a cloudflare origin server
-		   if (!ip_in_range($RemoteAddress, $CloudflareIPRange)) continue;
-         
+         if (!ip_in_range($RemoteAddress, $CloudflareIPRange))
+            continue;
+
          Gdn::Request()->RemoteAddress($CloudflareClientIP);
          $CloudflareRequest = TRUE;
-			break;
+         break;
       }
 
-		// Let people know that the CF plugin is turned on.
-		if ($CloudflareRequest && !headers_sent())
-		   header("X-CF-Powered-By: CF-Vanilla v".$this->GetPluginKey('Version'));
-      
+      // Let people know that the CF plugin is turned on.
+      if ($CloudflareRequest && !headers_sent())
+         header("X-CF-Powered-By: CF-Vanilla v" . $this->GetPluginKey('Version'));
    }
-   
+
 }
