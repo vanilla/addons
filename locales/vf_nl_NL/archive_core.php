a0e91f5c35a4957d430ae7006c139e6d6f3a530d
Todd Burry <todd@vanillaforums.com>
2012-10-11 10:53:00 -0400

    Remove the locales from the add ons repo.

--- locales/vf_nl_NL/archive_core.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$Definition['GenderSuffix.First.f'] = 'v';
-$Definition['GenderSuffix.First.m'] = 'm';
-$Definition['GenderSuffix.Third.f'] = 'v';
-$Definition['GenderSuffix.Third.m'] = 'm';
