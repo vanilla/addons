2e9ca4617f42ced32bdaada6741d0f9094d50934
Todd Burry <todd@vanillaforums.com>
2012-10-25 10:28:03 -0400

    Bump quotes version to refresh the js.

--- plugins/Quotes/class.quotes.plugin.php
+++ plugins/Quotes/class.quotes.plugin.php
@@ -20,7 +20,7 @@
 $PluginInfo['Quotes'] = array(
     'Name' => 'Quotes',
     'Description' => "Adds an option to each comment for users to easily quote each other.",
-    'Version' => '1.6.5',
+    'Version' => '1.6.6',
     'MobileFriendly' => TRUE,
     'RequiredApplications' => array('Vanilla' => '2.1a'),
     'RequiredTheme' => FALSE,
