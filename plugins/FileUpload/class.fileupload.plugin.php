f7245b8bf8e7a8dc1965726a9c47e8cd04aad315
lincolnwebs <lincolnwebs@gmail.com>
2012-12-19 17:31:40 -0500

    FileUpload default permissions

--- plugins/FileUpload/class.fileupload.plugin.php
+++ plugins/FileUpload/class.fileupload.plugin.php
@@ -27,7 +27,10 @@ $PluginInfo['FileUpload'] = array(
    'RequiredPlugins' => FALSE,
    'HasLocale' => FALSE,
    'MobileFriendly' => TRUE,
-   'RegisterPermissions' => array('Plugins.Attachments.Upload.Allow','Plugins.Attachments.Download.Allow'),
+   'RegisterPermissions' => array(
+      'Plugins.Attachments.Upload.Allow' => 'Garden.Profiles.Edit',
+      'Plugins.Attachments.Download.Allow' => 'Garden.Profiles.Edit'
+   ),
    //'SettingsUrl' => '/dashboard/plugin/fileupload',
    //'SettingsPermission' => 'Garden.Settings.Manage',
    'Author' => "Tim Gunter",
