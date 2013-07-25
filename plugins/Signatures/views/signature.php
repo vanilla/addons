2da002b3214c53eaec5650c6d26532317d7b6beb
Tim Gunter <tim@vanillaforums.com>
2013-07-25 18:18:57 -0400

    Signatures: Allow access to view controls even with no edit permission

--- plugins/Signatures/views/signature.php
+++ plugins/Signatures/views/signature.php
@@ -21,14 +21,15 @@ echo $this->Form->Errors();
          echo $this->Form->CheckBox('Plugin.Signatures.HideImages','Strip images out of signatures');
       ?>
    </li>
+   <?php if ($this->Data('CanEdit')): ?>
    <li>
       <?php
          echo $this->Form->Label('Signature Code', 'Plugin.Signatures.Sig');
          echo $this->Form->BodyBox('Body');
-         echo T('You can use HTML in your signature.', 'You can use <b><a href="http://htmlguide.drgrog.com/cheatsheet.php" target="_new">Simple Html</a></b> in your signature.');
 //         echo Wrap($this->Form->TextBox('Plugin.Signatures.Sig', array('MultiLine' => TRUE)), 'div', array('class' => 'TextBoxWrapper'));
       ?>
    </li>
+   <?php endif; ?>
    
    <?php
       $this->FireEvent('EditMySignatureAfter');
