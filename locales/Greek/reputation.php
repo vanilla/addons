58cb6c1828bb68974914f235683f93e4cc55564f
lincolnwebs <lincolnwebs@gmail.com>
2014-05-04 16:56:11 -0400

    Locales moved to separate repo

--- locales/Greek/reputation.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-
-$Definition['%s Point'] = "%s Βαθμός";
-$Definition['%s Points'] = "%s Βαθμοί";
-$Definition["All Time Leaders"] = "Πάντα ηγέτες";
-$Definition['HeadlineFormat.Badge'] = "{ActivityUserID,You} κέρδισε το Βραβείο <a href='{Url,html}'>{Data.Name,text}</a>.";
-$Definition['Points'] = "Βαθμοί";
-$Definition["This Week's Leaders"] = "Οι ηγέτες της εβδομάδας";
--- locales/vf_fi_FL/archive_core.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$Definition['GenderSuffix.First.f'] = '&nbsp;';
-$Definition['GenderSuffix.First.m'] = '&nbsp;';
-$Definition['GenderSuffix.Third.f'] = '&nbsp;';
-$Definition['GenderSuffix.Third.m'] = '&nbsp;';
