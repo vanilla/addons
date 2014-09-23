b8b7120f25dac71e01daad6f126f9b004903b4e7
Tim Gunter <tim@vanillaforums.com>
2014-09-23 15:42:03 -0400

    CloudflareSupport: Update code style to meet new interim standards

--- plugins/CloudflareSupport/class.cloudflaresupport.plugin.php
+++ plugins/CloudflareSupport/class.cloudflaresupport.plugin.php
@@ -1,26 +1,14 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php
 
 /**
- * Cloudflare Support Plugin
- *
- * This plugin inspect the incoming request headers and applies CF-Connecting-IP
- * to the request object.
- *
- * Changes:
- *  1.0     Initial release
- *  1.2     Fix bad method call
- *  1.2.1   Fix bad method call again
- *
- * @author Tim Gunter <tim@vanillaforums.com>
  * @copyright 2003 Vanilla Forums, Inc
  * @license http://www.opensource.org/licenses/gpl-2.0.php GPL
- * @package Addons
  */
 
 // Define the plugin:
 $PluginInfo['CloudflareSupport'] = array(
    'Description' => 'This plugin modifies the Request object to work with Cloudflare.',
-   'Version' => '1.2.1',
+   'Version' => '1.2.2',
    'RequiredApplications' => array('Vanilla' => '2.1'),
    'RequiredTheme' => FALSE,
    'RequiredPlugins' => FALSE,
@@ -32,6 +20,21 @@ $PluginInfo['CloudflareSupport'] = array(
    'AuthorUrl' => 'http://www.vanillaforums.com'
 );
 
+/**
+ * Cloudflare Support Plugin
+ *
+ * This plugin inspect the incoming request headers and applies CF-Connecting-IP
+ * to the request object.
+ *
+ * Changes:
+ *  1.0     Initial release
+ *  1.2     Fix bad method call
+ *  1.2.1   Fix bad method call again
+ *  1.2.2   Update CF IP list
+ *
+ * @author Tim Gunter <tim@vanillaforums.com>
+ * @package Addons
+ */
 class CloudflareSupportPlugin extends Gdn_Plugin {
 
    // CloudFlare IP ranges listed at https://www.cloudflare.com/ips
@@ -48,23 +51,26 @@ class CloudflareSupportPlugin extends Gdn_Plugin {
       "197.234.240.0/22",
       "198.41.128.0/17",
       "162.158.0.0/15",
-      "104.16.0.0/12");
+      "104.16.0.0/12"
+    );
 
    public function __construct() {
       parent::__construct();
 
       // If cloudflare isn't telling us a client IP, bust outta here!
-      $CloudflareClientIP = GetValue('HTTP_CF_CONNECTING_IP', $_SERVER, NULL);
-      if (is_null($CloudflareClientIP))
+      $CloudflareClientIP = val('HTTP_CF_CONNECTING_IP', $_SERVER, NULL);
+      if (is_null($CloudflareClientIP)) {
          return;
+      }
 
       $RequestAddress = Gdn::Request()->RequestAddress();
       $CloudflareRequest = FALSE;
       foreach ($this->CloudflareSourceIPs as $CloudflareIPRange) {
 
          // Not a cloudflare origin server
-         if (!ip_in_range($RequestAddress, $CloudflareIPRange))
+         if (!ip_in_range($RequestAddress, $CloudflareIPRange)) {
             continue;
+         }
 
          Gdn::Request()->RequestAddress($CloudflareClientIP);
          $CloudflareRequest = TRUE;
@@ -72,8 +78,9 @@ class CloudflareSupportPlugin extends Gdn_Plugin {
       }
 
       // Let people know that the CF plugin is turned on.
-      if ($CloudflareRequest && !headers_sent())
+      if ($CloudflareRequest && !headers_sent()) {
          header("X-CF-Powered-By: CF-Vanilla v" . $this->GetPluginKey('Version'));
+      }
    }
 
 }
