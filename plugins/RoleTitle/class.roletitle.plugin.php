a42fba4c628f5810a189459614b60c41a35ad817
beckyvb <beckyvanbussel@gmail.com>
2014-10-01 18:44:06 -0400

    Role Title: Check answers array in addition to comments array to set user roles.

--- plugins/RoleTitle/class.roletitle.plugin.php
+++ plugins/RoleTitle/class.roletitle.plugin.php
@@ -79,8 +79,14 @@ class RoleTitlePlugin extends Gdn_Plugin {
 		if (property_exists($Sender, 'Discussion')) {
 			$JoinDiscussion = array($Sender->Discussion);
 			RoleModel::SetUserRoles($JoinDiscussion, 'InsertUserID');
-         $Comments = $Sender->Data('Comments');
+            $Comments = $Sender->Data('Comments');
 			RoleModel::SetUserRoles($Comments->Result(), 'InsertUserID');
+
+            $Answers = $Sender->Data('Answers');
+            if (is_array($Answers)) {
+                RoleModel::SetUserRoles($Answers, 'InsertUserID');
+            }
+
          // And add the css class to the discussion
          if (is_array($Sender->Discussion->Roles)) {
             if (count($Sender->Discussion->Roles)) {
