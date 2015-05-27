63def8f342ac5b8cf15ba7f6a2c71a54f10a2d4c
beckyvb <beckyvanbussel@gmail.com>
2015-05-27 11:25:30 -0400

    AgeGate: Add optional confirmation of consent.

--- plugins/AgeGate/views/settings.php
+++ plugins/AgeGate/views/settings.php
@@ -5,14 +5,17 @@ echo $this->Form->Errors();
 ?>
 
 <div class="Info">
-    This plugin adds 'Date of Birth' to the registration forms.  Users must be at least the age below to complete the
-    registration process.
+    This plugin adds 'Date of Birth' to the registration forms. Users must be at least the age below to complete the
+    registration process. Alternatively, you can allow underage users to register with a confirmation of consent.
 </div>
 
 <ul>
     <li>
         <?php echo $this->Form->Label('Minimum Age', 'MinimumAge');  ?>
         <?php echo $this->Form->TextBox('MinimumAge', array('Class' => 'SmallInput')); ?>
+    </li>
+    <li>
+        <?php echo $this->Form->CheckBox('AddConfirmation', 'Allow underage users to register with a confirmation of consent');  ?>
     </li>
 </ul>
 
