d047e3b7a99aa5874cf38d207d85c9c6681d8dd1
lincolnwebs <lincolnwebs@gmail.com>
2015-11-16 14:32:40 -0500

    Casen Disqus view

--- plugins/Disqus/views/settings.php
+++ plugins/Disqus/views/settings.php
@@ -1,21 +1,21 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 
-<h1><?php echo $this->Data('Title'); ?></h1>
+<h1><?php echo $this->data('Title'); ?></h1>
 
 <div class="PageInfo">
-    <?php echo T('The Disqus plugin allows users to sign in using their Disqus account.', 'The Disqus plugin allows users to sign in using their Disqus account. <b>You must register your application with Disqus for this plugin to work.</b>'); ?>
+    <?php echo t('The Disqus plugin allows users to sign in using their Disqus account.', 'The Disqus plugin allows users to sign in using their Disqus account. <b>You must register your application with Disqus for this plugin to work.</b>'); ?>
 </div>
 
 <?php
 $Form = $this->Form;
 
-echo $Form->Open();
-echo $Form->Errors();
+echo $Form->open();
+echo $Form->errors();
 
-echo $Form->Simple(array(
+echo $Form->simple(array(
     'AuthenticationKey' => array('LabelCode' => 'Consumer Key', 'Options' => array('class' => 'InputBox WideInput')),
     'AssociationSecret' => array('LabelCode' => 'Consumer Secret', 'Options' => array('class' => 'InputBox WideInput'))
 ));
 
-echo $Form->Button('Save');
-echo $Form->Close();
+echo $Form->button('Save');
+echo $Form->close();
