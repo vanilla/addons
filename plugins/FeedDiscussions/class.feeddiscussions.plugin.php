e3560828cc7b77bdff715ab2d55d487b9c53a1f3
lincolnwebs <lincolnwebs@gmail.com>
2016-05-31 09:53:17 -0400

    Update plugin enabled checks

--- plugins/FeedDiscussions/class.feeddiscussions.plugin.php
+++ plugins/FeedDiscussions/class.feeddiscussions.plugin.php
@@ -354,7 +354,7 @@ class FeedDiscussionsPlugin extends Gdn_Plugin {
             );
          
          // Post as Minion (if one exists) or the system user
-         if (Gdn::PluginManager()->CheckPlugin('Minion')) {
+         if (Gdn::addonManager()->isEnabled('Minion', \Vanilla\Addon::TYPE_ADDON)) {
             $Minion = Gdn::PluginManager()->GetPluginInstance('MinionPlugin');
             $InsertUserID = $Minion->GetMinionUserID();
          }
