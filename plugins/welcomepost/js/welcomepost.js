f36de52d6be326a6991af939303258071260a6a0
DaazKu <daazku@gmail.com>
2016-07-08 17:20:28 -0400

    Temporary patch to make the popup appear on staging with bootstrap 3

--- plugins/welcomepost/js/welcomepost.js
+++ plugins/welcomepost/js/welcomepost.js
@@ -1,3 +1,9 @@
 $(function() {
     $.popup({}, gdn.getMeta('WelcomePostPopupMessage'));
+
+    // Patch for Scope / Bootstrap 3
+    $(document).trigger('popupLoading');
+    setTimeout(function () {
+        $(document).trigger('popupReveal');
+    }, 150);
 });
