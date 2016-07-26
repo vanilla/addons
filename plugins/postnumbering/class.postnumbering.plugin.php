0771924c43739469117723aa7f679f41316468f9
DaazKu <daazku@gmail.com>
2016-07-26 17:49:14 -0400

    Add post numbering v1.0

---
+++ plugins/postnumbering/class.postnumbering.plugin.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PostNumbering Plugin
+ *
+ * @copyright 2009-2016 Vanilla Forums Inc.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
+ */
+
+$PluginInfo['postnumbering'] = [
+    'Name' => 'Post Numbering',
+    'Description' => 'Add numerical indices, beside posts, that link to the post itself.',
+    'Version' => '1.0',
+    'RequiredApplications' => ['Vanilla' => '2.2'],
+    'License' => 'GNU GPL2',
+    'SettingsUrl' => '/settings/roletracker',
+    'SettingsPermission' => 'Garden.Settings.Manage',
+    'Author' => 'Alexandre (DaazKu) Chouinard',
+    'AuthorEmail' => 'alexandre.c@vanillaforums.com',
+    'AuthorUrl' => 'https://github.com/DaazKu',
+];
+
+/**
+ * Class PostNumberingPlugin
+ */
+class PostNumberingPlugin extends Gdn_Plugin {
+    /**
+     *
+     *
+     * @param DiscussionController $sender Sending controller instance.
+     * @param array $args Event arguments.
+     */
+    public function discussionController_discussionInfo_handler($sender, $args) {
+        echo wrap(
+            anchor('#1', discussionUrl($args['Discussion'])),
+            'span',
+            ['Class' => 'MItem PostNumbering Num-1']
+        );
+    }
+
+        /**
+     *
+     *
+     * @param DiscussionController $sender Sending controller instance.
+     * @param array $args Event arguments.
+     */
+    public function discussionController_commentInfo_handler($sender, $args) {
+        static $number = 2;
+
+        echo wrap(
+            anchor('#'.$number, commentUrl($args['Comment'])),
+            'span',
+            ['Class' => 'MItem PostNumbering Num-'.$number]
+        );
+
+        $number += 1;
+    }
+}
