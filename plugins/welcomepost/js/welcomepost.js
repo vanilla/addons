75a93cf107bd778d87dd1a17eff042b6ec873701
DaazKu <daazku@gmail.com>
2016-08-08 17:20:12 -0400

    Remove the WelcomePost patch about boostrap3 popups.

--- plugins/welcomepost/js/welcomepost.js
+++ plugins/welcomepost/js/welcomepost.js
@@ -1,9 +1,3 @@
 $(function() {
     $.popup({}, gdn.getMeta('WelcomePostPopupMessage'));
-
-    // Patch for Scope / Bootstrap 3
-    $(document).trigger('popupLoading');
-    setTimeout(function () {
-        $(document).trigger('popupReveal');
-    }, 150);
 });
