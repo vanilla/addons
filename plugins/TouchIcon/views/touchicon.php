b3b0d0129f601e1b19385d5e72526175d7cd7edd
beckyvb <beckyvanbussel@gmail.com>
2016-08-15 12:38:46 -0400

    Dashboard v3: Touch Icon settings page.

--- plugins/TouchIcon/views/touchicon.php
+++ plugins/TouchIcon/views/touchicon.php
@@ -1,34 +1,23 @@
 <?php if (!defined('APPLICATION')) exit();
-$Session = Gdn::Session();
+$Session = Gdn::session();
 ?>
 <h1><?php echo T('Touch Icon'); ?></h1>
 <?php
-echo $this->Form->Open(array('enctype' => 'multipart/form-data'));
-echo $this->Form->Errors();
-?>
-<ul>
-   <li>
+echo $this->Form->open(array('enctype' => 'multipart/form-data'));
+echo $this->Form->errors();
+echo wrap(t('TouchIconInfo', 'The touch icon appears when you bookmark a website on the homescreen of an Apple device.
+         These are usually 57x57 or 114x114 pixels. Apple adds rounded corners and lighting effect automatically.'),
+    'div',
+    ['class' => 'alert alert-info padded']);
+echo wrap(img(val('Path', $this->Data)), 'div'); ?>
+<div class="form-group row">
+   <div class="label-wrap">
       <?php
-         echo $this->Form->Label('Touch Icon', 'TouchIcon');
-         echo Wrap(
-               T('TouchIconInfo', 'The touch icon appears when you bookmark a website on the homescreen of an Apple device.
-                  These are usually 57x57 or 114x114 pixels. Apple adds rounded corners and lighting effect automatically.'),
-               'div',
-               array('class' => 'Info')
-            );
-
-         echo Wrap(
-            Img(val('Path', $this->Data)),
-            'div'
-         );
-         echo Wrap(
-            T('TouchIconEdit', 'Browse for a new touch icon to change it:'),
-            'div',
-            array('class' => 'Info')
-         );
-
-         echo $this->Form->Input('TouchIcon', 'file');
-      ?>
-   </li>
-</ul>
-<?php echo $this->Form->Close('Save');
+      echo $this->Form->label('Touch Icon', 'TouchIcon');
+      echo wrap(t('TouchIconEdit', 'Browse for a new touch icon to change it.'), 'div', ['class' => 'info']); ?>
+   </div>
+   <?php echo $this->Form->fileUploadWrap('TouchIcon'); ?>
+</div>
+<div class="js-modal-footer form-footer">
+   <?php echo $this->Form->close('Save'); ?>
+</div>
