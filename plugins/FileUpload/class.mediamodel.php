610ffc0bdbbfa75ed89f829c892e1b0b1f432b1d
lincolnwebs <lincolnwebs@gmail.com>
2016-08-25 14:05:20 -0400

    Coding standards: MediaModel

--- plugins/FileUpload/class.mediamodel.php
+++ plugins/FileUpload/class.mediamodel.php
@@ -24,16 +24,14 @@ class MediaModel extends Gdn_Model {
      * @param array $Options Not used.
      * @return array|false Returns the media row or **false** if it isn't found.
      */
-    public function GetID($MediaID, $DatasetType = false, $Options = []) {
-        $this->FireEvent('BeforeGetID');
+    public function fetID($MediaID, $DatasetType = false, $Options = []) {
+        $this->fireEvent('BeforeGetID');
         $Data = $this->SQL
-            ->Select('m.*')
-            //->Select('iu.*')
-            ->From('Media m')
-            //->Join('User iu', 'm.InsertUserID = iu.UserID', 'left') // Insert user
-            ->Where('m.MediaID', $MediaID)
-            ->Get()
-            ->FirstRow();
+            ->select('m.*')
+            ->from('Media m')
+            ->where('m.MediaID', $MediaID)
+            ->get()
+            ->firstRow();
 		
 		return $Data;
     }
@@ -45,15 +43,15 @@ class MediaModel extends Gdn_Model {
      * @param string $ForeignTable Lowercase.
      * @return object SQL results.
      */
-    public function Reassign($ForeignID, $ForeignTable, $NewForeignID, $NewForeignTable) {
-        $this->FireEvent('BeforeReassign');
+    public function reassign($ForeignID, $ForeignTable, $NewForeignID, $NewForeignTable) {
+        $this->fireEvent('BeforeReassign');
         $Data = $this->SQL
-            ->Update('Media')
-            ->Set('ForeignID', $NewForeignID)
-            ->Set('ForeignTable', $NewForeignTable)
-            ->Where('ForeignID', $ForeignID)
-            ->Where('ForeignTable', $ForeignTable)
-            ->Put();
+            ->update('Media')
+            ->set('ForeignID', $NewForeignID)
+            ->set('ForeignTable', $NewForeignTable)
+            ->where('ForeignID', $ForeignID)
+            ->where('ForeignTable', $ForeignTable)
+            ->put();
 		
 		return $Data;
     }
@@ -64,72 +62,64 @@ class MediaModel extends Gdn_Model {
      * @param string $Path Path to file.
      * @return array [0] => Height, [1] => Width.
      */
-    public static function GetImageSize($Path) {
+    public static function getImageSize($Path) {
         // Static FireEvent for intercepting non-local files.
         $Sender = new stdClass();
         $Sender->Returns = array();
         $Sender->EventArguments = array();
         $Sender->EventArguments['Path'] =& $Path;
-        $Sender->EventArguments['Parsed'] = Gdn_Upload::Parse($Path);
-        Gdn::PluginManager()->CallEventHandlers($Sender, 'Gdn_Upload', 'CopyLocal');
+        $Sender->EventArguments['Parsed'] = Gdn_Upload::parse($Path);
+        Gdn::pluginManager()->callEventHandlers($Sender, 'Gdn_Upload', 'CopyLocal');
     
-        if (!in_array(strtolower(pathinfo($Path, PATHINFO_EXTENSION)), array('gif', 'jpg', 'jpeg', 'png')))
+        if (!in_array(strtolower(pathinfo($Path, PATHINFO_EXTENSION)), array('gif', 'jpg', 'jpeg', 'png'))) {
             return array(0, 0);
+        }
 
         $ImageSize = @getimagesize($Path);
         if (is_array($ImageSize)) {
-            if (!in_array($ImageSize[2], array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG)))
+            if (!in_array($ImageSize[2], array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG))) {
                 return array(0, 0);
+            }
             
             return array($ImageSize[0], $ImageSize[1]);
         }
         
         return array(0, 0);
     }
-    
-    public function PreloadDiscussionMedia($DiscussionID, $CommentIDList) {
-        $this->FireEvent('BeforePreloadDiscussionMedia');
+
+    /**
+     *
+     *
+     * @param $DiscussionID
+     * @param $CommentIDList
+     * @return Gdn_DataSet
+     * @throws Exception
+     */
+    public function preloadDiscussionMedia($DiscussionID, $CommentIDList) {
+        $this->fireEvent('BeforePreloadDiscussionMedia');
         
-        $StartT = microtime(true);
         $Data = $this->SQL
-            ->Select('m.*')
-            ->From('Media m')
-            ->BeginWhereGroup()
-                ->Where('m.ForeignID', $DiscussionID)
-                ->Where('m.ForeignTable', 'discussion')
-            ->EndWhereGroup()
-            ->OrOp()
-            ->BeginWhereGroup()
-                ->WhereIn('m.ForeignID', $CommentIDList)
-                ->Where('m.ForeignTable', 'comment')
-            ->EndWhereGroup()
-            ->Get();
+            ->select('m.*')
+            ->from('Media m')
+            ->beginWhereGroup()
+                ->where('m.ForeignID', $DiscussionID)
+                ->where('m.ForeignTable', 'discussion')
+            ->endWhereGroup()
+            ->orOp()
+            ->beginWhereGroup()
+                ->whereIn('m.ForeignID', $CommentIDList)
+                ->where('m.ForeignTable', 'comment')
+            ->endWhereGroup()
+            ->get();
 
         // Assign image heights/widths where necessary.
-        $Data2 = $Data->Result();
+        $Data2 = $Data->result();
         foreach ($Data2 as &$Row) {
-            if ($Row->ImageHeight === NULL || $Row->ImageWidth === NULL) {
-                list($Row->ImageWidth, $Row->ImageHeight) = self::GetImageSize(MediaModel::PathUploads().'/'.ltrim($Row->Path, '/'));
-                $this->SQL->Put('Media', array('ImageWidth' => $Row->ImageWidth, 'ImageHeight' => $Row->ImageHeight), array('MediaID' => $Row->MediaID));
+            if ($Row->ImageHeight === null || $Row->ImageWidth === null) {
+                list($Row->ImageWidth, $Row->ImageHeight) = self::getImageSize(MediaModel::pathUploads().'/'.ltrim($Row->Path, '/'));
+                $this->SQL->put('Media', array('ImageWidth' => $Row->ImageWidth, 'ImageHeight' => $Row->ImageHeight), array('MediaID' => $Row->MediaID));
             }
         }
-/*
-        $DiscussionData = $this->SQL
-            ->Select('m.*')
-            ->From('Media m')
-            ->Where('m.ForeignID', $DiscussionID)
-            ->Where('m.ForeignTable', 'discussion')
-            ->Get()->Result(DATASET_TYPE_ARRAY);
-
-        $CommentData = $this->SQL
-            ->Select('m.*')
-            ->From('Media m')
-            ->WhereIn('m.ForeignID', $CommentIDList)
-            ->Where('m.ForeignTable', 'comment')
-            ->Get()->Result(DATASET_TYPE_ARRAY);
-        
-        $Data = array_merge($DiscussionData, $CommentData);
-*/
 
 		return $Data;
     }
@@ -141,45 +131,56 @@ class MediaModel extends Gdn_Model {
      * @param array|bool $Options Either a boolean that says whether or not to delete the file or an array with a
      * **Delete** key.
      */
-    public function Delete($Media = [], $Options = []) {
+    public function delete($Media = [], $Options = []) {
         if (is_bool($Options)) {
             $DeleteFile = $Options;
         } else {
             $DeleteFile = val('Delete', $Options, true);
         }
 
-        $MediaID = FALSE;
-        if (is_a($Media, 'stdClass'))
+        $MediaID = false;
+        if (is_a($Media, 'stdClass')) {
             $Media = (array)$Media;
+        }
                 
-        if (is_numeric($Media)) 
+        if (is_numeric($Media)) {
             $MediaID = $Media;
-        elseif (array_key_exists('MediaID', $Media))
+        }
+        elseif (array_key_exists('MediaID', $Media)) {
             $MediaID = $Media['MediaID'];
+        }
         
         if ($MediaID) {
             $Media = $this->GetID($MediaID);
-            $this->SQL->Delete($this->Name, array('MediaID' => $MediaID), FALSE);
+            $this->SQL->Delete($this->Name, array('MediaID' => $MediaID), false);
             
             if ($DeleteFile) {
-                $DirectPath = MediaModel::PathUploads().DS.GetValue('Path',$Media);
-                if (file_exists($DirectPath))
+                $DirectPath = MediaModel::pathUploads().DS.val('Path',$Media);
+                if (file_exists($DirectPath)) {
                     @unlink($DirectPath);
+                }
             }
         } else {
-            $this->SQL->Delete($this->Name, $Media, FALSE);
+            $this->SQL->delete($this->Name, $Media, false);
         }
     }
-    
-    public function DeleteParent($ParentTable, $ParentID) {
-        $MediaItems = $this->SQL->Select('*')
-            ->From($this->Name)
-            ->Where('ForeignTable', strtolower($ParentTable))
-            ->Where('ForeignID', $ParentID)
-            ->Get()->Result(DATASET_TYPE_ARRAY);
+
+    /**
+     *
+     *
+     * @param $ParentTable
+     * @param $ParentID
+     */
+    public function deleteParent($ParentTable, $ParentID) {
+        $MediaItems = $this->SQL
+            ->select('*')
+            ->from($this->Name)
+            ->where('ForeignTable', strtolower($ParentTable))
+            ->where('ForeignID', $ParentID)
+            ->get()->result(DATASET_TYPE_ARRAY);
             
         foreach ($MediaItems as $Media) {
-            $this->Delete(GetValue('MediaID',$Media));
+            $this->delete(val('MediaID',$Media));
         }
     }
     
@@ -188,81 +189,110 @@ class MediaModel extends Gdn_Model {
      *
      * @return string Path to upload folder.
      */
-    public static function PathUploads() {
-        if (defined('PATH_LOCAL_UPLOADS'))
+    public static function pathUploads() {
+        if (defined('PATH_LOCAL_UPLOADS')) {
             return PATH_LOCAL_UPLOADS;
-        else
-            return PATH_UPLOADS;
+        }
+
+        return PATH_UPLOADS;
     }
 
-    public static function ThumbnailHeight() {
-        static $Height = FALSE;
+    /**
+     *
+     *
+     * @return bool|mixed
+     */
+    public static function thumbnailHeight() {
+        static $Height = false;
 
-        if ($Height === FALSE)
-            $Height = C('Plugins.FileUpload.ThumbnailHeight', 128);
+        if ($Height === false) {
+            $Height = c('Plugins.FileUpload.ThumbnailHeight', 128);
+        }
+
         return $Height;
     }
 
-    public static function ThumbnailWidth() {
-        static $Width = FALSE;
+    /**
+     *
+     *
+     * @return bool|mixed
+     */
+    public static function thumbnailWidth() {
+        static $Width = false;
 
-        if ($Width === FALSE)
-            $Width = C('Plugins.FileUpload.ThumbnailWidth', 256);
+        if ($Width === false) {
+            $Width = c('Plugins.FileUpload.ThumbnailWidth', 256);
+        }
+
         return $Width;
     }
 
-    public static function ThumbnailUrl(&$Media) {
-        if (GetValue('ThumbPath', $Media))
-            return Gdn_Upload::Url(ltrim(GetValue('ThumbPath', $Media), '/'));
+    /**
+     *
+     *
+     * @param $Media
+     * @return mixed|string
+     */
+    public static function thumbnailUrl(&$Media) {
+        if (val('ThumbPath', $Media))
+            return Gdn_Upload::url(ltrim(val('ThumbPath', $Media), '/'));
         
-        $Width = GetValue('ImageWidth', $Media);
-        $Height = GetValue('ImageHeight', $Media);
+        $Width = val('ImageWidth', $Media);
+        $Height = val('ImageHeight', $Media);
 
         if (!$Width || !$Height) {
-            if ($Height = self::ThumbnailHeight())
-                SetValue('ThumbHeight', $Media, $Height);
+            if ($Height = self::thumbnailHeight())
+                setValue('ThumbHeight', $Media, $Height);
             return '/plugins/FileUpload/images/file.png';
         }
 
-        $RequiresThumbnail = FALSE;
-        if (self::ThumbnailHeight() && $Height > self::ThumbnailHeight())
-            $RequiresThumbnail = TRUE;
-        elseif (self::ThumbnailWidth() && $Width > self::ThumbnailWidth())
-            $RequiresThumbnail = TRUE;
+        $RequiresThumbnail = false;
+        if (self::thumbnailHeight() && $Height > self::thumbnailHeight())
+            $RequiresThumbnail = true;
+        elseif (self::thumbnailWidth() && $Width > self::thumbnailWidth())
+            $RequiresThumbnail = true;
 
-        $Path = ltrim(GetValue('Path', $Media), '/');
+        $Path = ltrim(val('Path', $Media), '/');
         if ($RequiresThumbnail) {
-            $Result = Url('/utility/thumbnail/'.GetValue('MediaID', $Media, 'x').'/'.$Path, TRUE);
+            $Result = url('/utility/thumbnail/'.val('MediaID', $Media, 'x').'/'.$Path, true);
         } else {
-            $Result = Gdn_Upload::Url($Path);
+            $Result = Gdn_Upload::url($Path);
         }
+
         return $Result;
     }
 
-    public static function Url($Media) {
-        static $UseDownloadUrl = NULL;
-        if ($UseDownloadUrl === NULL)
-            $UseDownloadUrl = C('Plugins.FileUpload.UseDownloadUrl');
+    /**
+     *
+     *
+     * @param $Media
+     * @return mixed|string
+     */
+    public static function url($Media) {
+        static $UseDownloadUrl = null;
 
-//        decho($Media);
-        
+        if ($UseDownloadUrl === null) {
+            $UseDownloadUrl = c('Plugins.FileUpload.UseDownloadUrl');
+        }
+
         if (is_string($Media)) {
             $SubPath = $Media;
-            if (method_exists('Gdn_Upload', 'Url'))
-                $Url = Gdn_Upload::Url("$SubPath");
-            else
+            if (method_exists('Gdn_Upload', 'Url')) {
+                $Url = Gdn_Upload::url("$SubPath");
+            } else {
                 $Url = "/uploads/$SubPath";
+            }
         } elseif ($UseDownloadUrl) {
-            $Url = '/discussion/download/'.GetValue('MediaID', $Media).'/'.rawurlencode(GetValue('Name', $Media));
+            $Url = '/discussion/download/'.val('MediaID', $Media).'/'.rawurlencode(val('Name', $Media));
         } else {
-            $SubPath = ltrim(GetValue('Path', $Media), '/');
-            if (method_exists('Gdn_Upload', 'Url'))
-                $Url = Gdn_Upload::Url("$SubPath");
-            else
+            $SubPath = ltrim(val('Path', $Media), '/');
+            if (method_exists('Gdn_Upload', 'Url')) {
+                $Url = Gdn_Upload::url("$SubPath");
+            } else {
                 $Url = "/uploads/$SubPath";
+            }
         }
 
         return $Url;
     }
-    
 }
