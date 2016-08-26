1bf9b7b9aa22d606fb8cbdb8448b82fc11800056
lincolnwebs <lincolnwebs@gmail.com>
2016-08-26 10:30:42 -0400

    Fix Invisibility Cloak coding standards

--- plugins/InvisibilityCloak/class.invisibilitycloak.plugin.php
+++ plugins/InvisibilityCloak/class.invisibilitycloak.plugin.php
@@ -1,35 +1,36 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php
+/**
+ * @copyright 2014-2016 Vanilla Forums, Inc.
+ */
 
 $PluginInfo['InvisibilityCloak'] = array(
-   'Name' => 'Invisibility Cloak',
-   'Description' => 'Hide your forum from the prying eyes of search engines and bots while you set it up.',
-   'Version' => '1.0',
-   'RequiredApplications' => array('Vanilla' => '2.1'),
-   'Author' => "Lincoln Russell",
-   'AuthorEmail' => 'lincoln@vanillaforums.com'
+    'Name' => 'Invisibility Cloak',
+    'Description' => 'Hide your forum from the prying eyes of search engines and bots while you set it up.',
+    'Version' => '1.0',
+    'RequiredApplications' => ['Vanilla' => '2.1'],
+    'Author' => "Lincoln Russell",
+    'AuthorEmail' => 'lincoln@vanillaforums.com'
 );
 
+/**
+ * Class InvisibilityCloakPlugin
+ */
 class InvisibilityCloakPlugin extends Gdn_Plugin {
-   /**
-    * robots.txt.
-    */
-   public function RootController_Robots_Create($Sender) {
-      header("Content-Type: text/plain");
-      echo "User-agent: *\nDisallow: /";
-   }
 
-   /**
-    * No bots meta tag.
-    */
-   public function Base_Render_Before($Sender) {
-      if ($Sender->Head)
-         $Sender->Head->AddTag('meta', array('name' => 'robots', 'content' => 'noindex,noarchive'));
-   }
+    /**
+     * robots.txt.
+     */
+    public function rootController_robots_create($sender) {
+        header("Content-Type: text/plain");
+        echo "User-agent: *\nDisallow: /";
+    }
 
-   /**
-    * Plugin setup.
-    */
-   public function Setup() {
-
-   }
+    /**
+     * No bots meta tag.
+     */
+    public function base_render_before($sender) {
+        if ($sender->Head) {
+            $sender->Head->addTag('meta', array('name' => 'robots', 'content' => 'noindex,noarchive'));
+        }
+    }
 }
