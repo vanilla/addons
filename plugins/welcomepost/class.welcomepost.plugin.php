6c64d16addd83f76cdf13ba5be0942a34a9ae4e4
Patrick Kelly <nervoustwit@users.noreply.github.com>
2017-01-30 17:07:51 -0500

    Get Target from POST for creating the “Skip” button in the Welcome Post plugin

--- plugins/welcomepost/class.welcomepost.plugin.php
+++ plugins/welcomepost/class.welcomepost.plugin.php
@@ -108,7 +108,8 @@ class WelcomePostPlugin extends Gdn_Plugin {
      */
     public function entryController_registrationSuccessful_handler($sender, $args) {
         if (!c('Garden.Registration.ConfirmEmail')) {
-            redirect('/post/discussion?welcomepost=true');
+            $target = (Gdn::request()->post('Target')) ? '&Target='.Gdn::request()->post('Target') : null;
+            redirect('/post/discussion?welcomepost=true'.$target);
         }
     }
 
@@ -119,7 +120,7 @@ class WelcomePostPlugin extends Gdn_Plugin {
      * @param UserModel $args
      */
     public function userModel_afterSignIn_handler($sender, $args) {
-        $target = (Gdn::request()->get('Target')) ? '&Target='.Gdn::request()->get('Target') : null;
+        $target = (Gdn::request()->post('Target')) ? '&Target='.Gdn::request()->post('Target') : null;
         // AfterSignIn event is fired in several places, the InsertUserID
         // argument is only passed in the connect script.
         if (val('InsertUserID', $args)) {
@@ -170,7 +171,7 @@ class WelcomePostPlugin extends Gdn_Plugin {
         Gdn::locale()->setTranslation('Cancel', t('Skip'));
 
         //Skipping will bring you to where you were going
-        $cancelURL = ($sender->Request->get('Target')) ? $sender->Request->get('Target') : '/';
+        $cancelURL = (Gdn::request()->get('Target')) ? Gdn::request()->get('Target') : '/';
         $sender->setData('_CancelUrl', $cancelURL);
 
         $username = val('Name', Gdn::session()->User, 'Unknown');
