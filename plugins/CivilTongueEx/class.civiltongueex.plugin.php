0c8dd058aa8dbf0ce1cde39e403e92982e4c380f
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-06-09 07:35:18 -0400

    Fixed variable casings

--- plugins/CivilTongueEx/class.civiltongueex.plugin.php
+++ plugins/CivilTongueEx/class.civiltongueex.plugin.php
@@ -384,8 +384,8 @@ class CivilTonguePlugin extends Gdn_Plugin {
      * @param $Sender
      * @param $Args
      */
-    public function activityModel_beforeSave_handler($Sender, &$Args) {
-        $activity = val('Activity', $Args);
+    public function activityModel_beforeSave_handler($sender, &$args) {
+        $activity = val('Activity', $args);
         setValue('Story', $activity, $this->replace(val('Story', $activity)));
     }
 
