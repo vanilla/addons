333e94e15cce005dd5bf9c36491c8f3936eb88f2
Chris Chabilall <chris.c@vanillaforums.com>
2019-02-11 14:24:13 -0500

    Added  variable

--- plugins/Ignore/class.ignore.plugin.php
+++ plugins/Ignore/class.ignore.plugin.php
@@ -354,7 +354,8 @@ class IgnorePlugin extends Gdn_Plugin {
       $userID = Gdn::session()->UserID;
       foreach ($recipients as $recipientID => $recipient) {
          if ($this->ignored($userID, $recipientID)) {
-            $sender->Form->addError(sprintf(t('Unable to send message, %s is ignoring you.'), htmlspecialchars($userID['Name'])));
+             $user = Gdn::userModel()->getID($recipientID, DATASET_TYPE_ARRAY);
+             $sender->Form->addError(sprintf(t('Unable to send message, %s is ignoring you.'), htmlspecialchars($user['Name'])));
          }
       }
 
