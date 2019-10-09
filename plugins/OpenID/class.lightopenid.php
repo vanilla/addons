bc16716f795531d1668ac27e2c77cbfa40277d3b
Patrick Kelly <patrick.k@vanillaforums.com>
2019-10-09 13:01:55 -0400

    Update OpenID curl option to not follow.

--- plugins/OpenID/class.lightopenid.php
+++ plugins/OpenID/class.lightopenid.php
@@ -147,7 +147,7 @@ class LightOpenID {
         $params = http_build_query($params, '', '&');
         $curl = curl_init($url.($method == 'GET' && $params ? '?'.$params : ''));
         curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
