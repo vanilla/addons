e11d77f2c18606c8f995119220ed93ede3622a1c
igraziatto <isis.graziatto@gmail.com>
2020-03-04 12:14:58 -0500

    qa

--- plugins/CivilTongueEx/class.civiltongueex.plugin.php
+++ plugins/CivilTongueEx/class.civiltongueex.plugin.php
@@ -221,9 +221,8 @@ class CivilTonguePlugin extends Gdn_Plugin {
         $discussions = $sender->data('Discussions', []);
         if (is_array($discussions) || $discussions instanceof \Traversable) {
             foreach ($discussions as &$discussion) {
-//                $discussion->Name = $this->replace($discussion->Name);
-//                $discussion->Body = $this->replace($discussion->Body);
                 $discussion->Name = $this->contentFilter->replace($discussion->Name);
+                $discussion->Body = $this->contentFilter->($discussion->Body);
             }
         }
     }
