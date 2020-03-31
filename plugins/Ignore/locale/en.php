460fb8f343b20e5052efee2c13208aabe612488e
Richard Flynn <richardflynn@gmail.com>
2020-03-31 15:16:23 -0400

    add locale file to ingore plugin

---
+++ plugins/Ignore/locale/en.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author Richard Flynn <richard.flynn@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+$Definition['Are you sure you want to unignore %s?'] = 'Are you sure you want to unignore <b>%s</b>?';
