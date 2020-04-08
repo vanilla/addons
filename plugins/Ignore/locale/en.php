c14e850ca3cf3090f536c92fe5a089d63a130cd7
Richard Flynn <richardflynn@gmail.com>
2020-04-08 15:00:57 -0400

    add translation strings

---
+++ plugins/Ignore/locale/en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Richard Flynn <richard.flynn@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+$Definition['IgnoreListMeter'] = 'Ignore list is <b>%s%%</b> full (<b>%d/%d</b>).';
+$Definition['IgnoreListRevoke'] = 'Revoke <b>%s</b>\'s ignore list privileges?';
+$Definition['IgnoreListUnlimited'] = '<b>Unlimited</b> list, ignored <b>%d</b> %s';
