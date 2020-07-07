78a439252bbf53ae6148afb9ce79520329d55974
Adam Charron <adam@charrondev.com>
2020-07-07 08:40:00 -0400

    Delete moved plugins

--- plugins/Sitemaps/js/sitemaps.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function () {
-    var SitemapsBuildURL = gdn.url('/plugin/sitemaps/build');
-    jQuery.ajax({
-        url: SitemapsBuildURL,
-        type: 'POST'
-    });
-});
--- plugins/jsconnect/views/jsconnect_error.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('APPLICATION')) exit(); ?>
-
-<h1><?php echo $this->data('Title'); ?></h1>
-<?php
-echo $this->Form->open();
-echo $this->Form->errors();
-echo $this->Form->close();
-?>
