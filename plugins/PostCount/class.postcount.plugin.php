947ede4775a0a595bd067255aae9a00fe0c56f81
Adam Charron <146114816+acharron-hl@users.noreply.github.com>
2025-11-07 10:33:11 -0500

    Fix warning in post count plugin

--- plugins/PostCount/class.postcount.plugin.php
+++ plugins/PostCount/class.postcount.plugin.php
@@ -1,4 +1,6 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php if (!defined("APPLICATION")) {
+    exit();
+}
 /*
 Copyright 2008, 2009 Vanilla Forums Inc.
 This file is part of Garden.
@@ -8,39 +10,54 @@ You should have received a copy of the GNU General Public License along with Gar
 Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
 */
 
-class PostCountPlugin extends Gdn_Plugin {
-   
-   public function userInfoModule_onBasicInfo_handler($sender) {
-      $user = Gdn::userModel()->getID($sender->User->UserID);
-      if ($user) {
-         $postCount = getValue('CountComments', $user, 0) + getValue('CountDiscussions', $user, 0);
-         echo "<dt class=\"Posts\">".t('Posts')."</dt>\n";
-         echo "<dd class=\"Posts\">".number_format($postCount)."</dd>";
-      }
-   }
-   
-   public function discussionController_authorInfo_handler($sender) {
-      $this->_AttachPostCount($sender);
-   }
-   
-   public function postController_authorInfo_handler($sender) {
-      $this->_AttachPostCount($sender);
-   }
-   
-   protected function _AttachPostCount($sender) {
-      $user = Gdn::userModel()->getID($sender->EventArguments['Author']->UserID);
-      if ($user) {
-         $posts = getValue('CountComments', $user, 0) + getValue('CountDiscussions', $user, 0);
-         echo '<span class="MItem PostCount">'.plural(number_format($posts), '@'.t('Posts.Singular: %s', 'Posts: <b>%s</b>'), '@'.t('Posts.Plural: %s', 'Posts: <b>%s</b>')).'</span>';
-      }
-   }
+class PostCountPlugin extends Gdn_Plugin
+{
+    public function userInfoModule_onBasicInfo_handler($sender)
+    {
+        $user = Gdn::userModel()->getID($sender->User->UserID);
+        if ($user) {
+            $postCount = getValue("CountComments", $user, 0) + getValue("CountDiscussions", $user, 0);
+            echo "<dt class=\"Posts\">" . t("Posts") . "</dt>\n";
+            echo "<dd class=\"Posts\">" . number_format($postCount) . "</dd>";
+        }
+    }
 
-   public function setup() {
-      // Nothing to do here!
-   }
-   
-   public function structure() {
-      // Nothing to do here!
-   }
-         
+    public function discussionController_authorInfo_handler($sender)
+    {
+        $this->_AttachPostCount($sender);
+    }
+
+    public function postController_authorInfo_handler($sender)
+    {
+        $this->_AttachPostCount($sender);
+    }
+
+    protected function _AttachPostCount($sender)
+    {
+        $userID = $sender->EventArguments["Author"]->UserID ?? null;
+        if ($userID === null) {
+            return;
+        }
+        $user = Gdn::userModel()->getID($sender->EventArguments["Author"]->UserID);
+        if ($user) {
+            $posts = getValue("CountComments", $user, 0) + getValue("CountDiscussions", $user, 0);
+            echo '<span class="MItem PostCount">' .
+                plural(
+                    number_format($posts),
+                    "@" . t("Posts.Singular: %s", "Posts: <b>%s</b>"),
+                    "@" . t("Posts.Plural: %s", "Posts: <b>%s</b>")
+                ) .
+                "</span>";
+        }
+    }
+
+    public function setup()
+    {
+        // Nothing to do here!
+    }
+
+    public function structure()
+    {
+        // Nothing to do here!
+    }
 }
