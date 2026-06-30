24c562e66897b392989573129d853349441b1003
Hilary Rallo <hrallo@higherlogic.com>
2026-06-30 11:52:18 -0500

    Remove NoIndex plugin, added to vanilla-cloud

--- plugins/NoIndex/locale/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @author Richard Flynn <richard.flynn@vanillaforums.com>
- * @copyright 2009-2020 Vanilla Forums Inc.
- * @license GPL-2.0-only
- */
-// phpcs:ignoreFile
-
-$Definition['Discussion marked as noindex'] = 'Discussion marked as noindex';
